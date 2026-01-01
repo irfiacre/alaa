b7010b263100d8d5535152c5ef158f960d8f7571
irfiacre <irfiacre@gmail.com>
2026-01-01 15:21:32 +0200

    Improve building summary dataset

--- utils/data_cleanup.py
+++ utils/data_cleanup.py
@@ -3,26 +3,9 @@
 """
 Approach:
 
-0. Understand the template to pass to the model.
-{
-  "instruction": "...",
-  "input": "....",
-  "output": "...",
-  "domain": "..._law",
-  "language": "en"
-}
-Format:
-{
-  "instruction": "Explain in legal terms",
-  "input": "On the topic <chapter title>, what does the official gazete say about <article title>",
-  "output": "<information about following the title>"
-}
-1. Process the PDF file.
 2. Finalize the dataset & Push it to hugging face.
 """
-
 import json
-from unittest import result
 import pypdf
 from enchant.checker import SpellChecker
 
@@ -33,7 +16,9 @@ english_spell_check = SpellChecker("en_GB")
 def validate_english_phrase(phrase:str)-> bool:
     english_spell_check.set_text(phrase)
     for err in english_spell_check:
-        if err.word:
+        if err.word and "rwanda" not in err.word:
+            with open("logs/log.txt", "a", encoding='utf-8') as log_file:
+                log_file.write(phrase + "\n")
             return False
     return True
 
@@ -44,10 +29,16 @@ def build_article_summary() -> list:
     part_title = ""
     chapter_title = "" 
     title_title = ""
+    section_title = ""
+    sub_section_title = ""
     result = []
-    for page in reader.pages[:12]:
+    for page in reader.pages[1:43]:
         page_text = page.extract_text()
         split_text = page_text.split("\n \n")
+        n = len(split_text)
+        diviser = len(split_text)//3
+        split_text = split_text[diviser+1: n - (diviser-3)]
+
         for text in split_text:
             temp = text.strip().split(":")
             if len(temp) <= 1:
@@ -57,21 +48,29 @@ def build_article_summary() -> list:
             text_phrase = f'{text_title}: {"".join(text_definition).capitalize()}'
             if not validate_english_phrase(text_phrase):
                 continue
-            if "part" in text_phrase.lower():
+            if "PART" in temp[0].strip():
                 part_title = text_phrase
                 continue
-            elif "title" in text_phrase.lower():
+            elif "TITLE" in temp[0].strip():
                 title_title = text_phrase
                 continue
-            elif "chapter" in text_phrase.lower():
+            elif "CHAPTER" in temp[0].strip():
                 chapter_title = text_phrase
+                continue
+            elif "subsection" in text_phrase.lower():
+                sub_section_title = text_phrase
+                continue
+            elif "section" in text_phrase.lower():
+                section_title = text_phrase
+                sub_section_title = ""
                 continue
             dataset_dict = {
                     "instruction": "Explain the summary of the law articles, as mentioned in the Rwandan Official Gazette no. Special of 27/09/2018",
-                    "input": f"What is {temp[0]} of {part_title}, {title_title}, {chapter_title}?",
+                    "input": f"What is {temp[0]} of {part_title}, {title_title}, {chapter_title}{' ,'+section_title if section_title else ''}, {' ,'+sub_section_title if sub_section_title else ''}?",
                     "output": f"{text_phrase}"
             }
             result.append(dataset_dict)
+    
     return result
 
 if __name__=="__main__":
